baf3cc7847f842eea2f01719c48b47d9fe1ea3fd
Hugo Josefson <hugo@josefson.org>
2009-06-17 13:47:18 +0200

    Updated install.* scripts for Android SDK 1.5_r2.

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile="%ANDROID_SDK%\android.jar" -DgeneratePom=true -DgroupId=android -DartifactId=android -Dversion=1.1_r1 -Dpackaging=jar
+mvn install:install-file -Dfile="%ANDROID_SDK_15%\platforms\android-1.5\android.jar" -DgeneratePom=true -DgroupId=android -DartifactId=android -Dversion=1.5_r2 -Dpackaging=jar
